9b2d5dcf597678ad0f9e6fbf94c682333d72c244
hechuan <hechuan@51tuodao.com>
2017-08-25 17:40:25 +0800

    重试增加

--- bp-api/bp-api-facade/src/main/java/com/tuodao/bp/api/facade/ApiFacadeApplication.java
+++ bp-api/bp-api-facade/src/main/java/com/tuodao/bp/api/facade/ApiFacadeApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients(basePackages= {"com.tuodao.bp.api.facade.service"})
